1eab2e444448d747e34fae22b4b189152072ffc5
Moritztz <tm.283wings@gmail.com>
2020-09-03 02:30:48 +0900

    一定時間内のスコアが高いデータを保持

--- js/gesture_detection.js
+++ js/gesture_detection.js
@@ -1,4 +1,18 @@
+const SAMPLING_DULATION = 400 //ms
 let last_time = 0;
+
+let initCount = 0;
+const MAX_INIT_COUNT = 20;
+let sum_delta_time = 0;
+let sampl_count = 0;
+
+let r_sho = 0;
+let r_elb = 0;
+let r_wri = 0;
+let l_sho = 0;
+let l_elb = 0;
+let l_wri = 0;
+
 ボタンの表示
 function gestureDetection(data) {
   /* data score, part:string, position.x/y, timestamp, score(all)
@@ -15,5 +29,62 @@ function gestureDetection(data) {
     let fps = 1000 / delta_time;
     $("#fps").text(parseInt(fps));
   }
+
+  if (last_time != 0 && sampl_count == 0) {
+    if (initCount < MAX_INIT_COUNT) {
+      sum_delta_time += delta_time;
+      initCount++;
+    } else {
+      let ave_delta_time = sum_delta_time / MAX_INIT_COUNT;
+      sampl_count = parseInt(SAMPLING_DULATION / ave_delta_time);
+
+      //初期化
+      l_sho = new ArrKeepData();
+      l_elb = new ArrKeepData();
+      l_wri = new ArrKeepData();
+      r_sho = new ArrKeepData();
+      r_elb = new ArrKeepData();
+      r_wri = new ArrKeepData();
+    }
+  } else if (sampl_count != 0) {
+    //データ更新
+    l_sho.add(data[5]);
+    l_elb.add(data[7]);
+    l_wri.add(data[9]);
+    r_sho.add(data[6]);
+    r_elb.add(data[8]);
+    r_wri.add(data[10]);
+  }
+
   last_time = data[17].timestamp;
+}
+
+//スコアが最も高いデータの位置x,y
+var ArrKeepData = function() {
+  this.keypoints = [0];
+  this.x = 0;
+  this.y = 0;
+
+  this.add = function(part){
+    this.keypoints.unshift(part);
+    if (this.keypoints.length() > sampl_count) {
+      part.pop();
+    }
+  }
+
+  this.updatePart = function() {
+    let index = 0;
+    let maxCallback = (( max, cur, cIndex) => {
+      if (max > cur) {
+        return max;
+      } else {
+        index = cIndex;
+        return cur;
+      }
+    });
+    this.keypoints.map( el => el.score )
+                  .reduce( maxCallback, -Infinity);
+    this.x = this.keypoints[index].position.x;
+    this.y = this.keypoints[index].position.y;
+  }
 }
